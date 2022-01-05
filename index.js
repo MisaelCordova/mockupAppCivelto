61e6735677ca705375df89937341104fc6e31880
Misael Cordova <misael.cordova.f@gmail.com>
2022-01-05 15:50:45 -0300

    add cilindro

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 let modal = document.getElementById("myModal");
 
 let btn = document.getElementById("btnModal");
+/*
+btn.addEventListener(function(e){
+  e.preventDefault();
+})*/
 
 btn.onclick = function () {
     modal.style.display = "block";
